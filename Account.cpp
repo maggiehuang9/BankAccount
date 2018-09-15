a00cadad8c6fc6c73a2869d021fc5a73c90aa772
Dade Huang <huangd@alphastarcorp.com>
2018-09-15 14:59:30 -0700

    remove pre-compile header and not-used functions

--- Account.cpp
+++ Account.cpp
@@ -1,4 +1,3 @@
-#include "stdafx.h"
 #include "Account.h"
 #include <iostream>
 using namespace std;
@@ -16,104 +15,22 @@ void Account::openAccount(int accountTotal)
 	cout << "Account number: " << number << endl;
 }
 
-void Account::balanceInquiry(int accountTotal) {
-	int accountNum;
-	cout << "Account number? " << endl;
-	cin >> accountNum;
-	for (int i = 0; i < accountTotal; i++) {
-		if (number == accountNum)
-		{
-			cout << "Account number: " << number << endl;
-			cout << "Balance: " << balance << endl;
-			break;
-		}
-	}
-}
-
 void Account::balanceInquiry() {
 	cout << "Account number: " << number << endl;
 	cout << "Balance: " << balance << endl;
-}
-
-void Account::deposit(int accountTotal) {
-	int accountNum;
-	double amount;
-	cout << "Account number? " << endl;
-	cin >> accountNum;
-	for (int i = 0; i < accountTotal; i++) {
-		if (number == accountNum)
-		{
-			cout << "Amount?" << endl;
-			cin >> amount;
-			balance += amount;
-			cout << "Account number: " << number << endl;
-			cout << "Balance: " << balance << endl;
-			break;
-		}
-	}
 }
 
 void Account::deposit(double amount) {
 	balance += amount;
 }
 
-void Account::withdrawal(int accountTotal) {
-	int accountNum;
-	double amount;
-	cout << "Account number? " << endl;
-	cin >> accountNum;
-	for (int i = 0; i < accountTotal; i++) {
-		if (number == accountNum)
-		{
-			cout << "Amount?" << endl;
-			cin >> amount;
-			if (amount <= balance) {
-				balance -= amount;
-				cout << "Account number: " << number << endl;
-				cout << "Balance: " << balance << endl;
-			}
-			else
-				cout << "Insufficient funds." << endl;
-			break;
-		}
-	}
-}
-
 void Account::withdrawal(double amount) {
 	balance -= amount;
-}
-
-void Account::closeAccount(int accountTotal) {
-	int accountNum;
-	cout << "Account number? " << endl;
-	cin >> accountNum;
-	for (int i = 0; i < accountTotal; i++) {
-		if (number == accountNum)
-		{
-			accountTotal--;
-			number = 0;
-			balance = 0;
-			break;
-		}
-	}
 }
 
 void Account::closeAccount2(int accountTotal) {
 	number = 0;
 	balance = 0;
-}
-
-
-void Account::interestRate(int accountTotal) {
-	double interestRate;
-	cout << "Interest rate?" << endl;
-	cin >> interestRate;
-	for (int i = 0; i < accountTotal; i++) {
-		if (balance > 0)
-		{
-			balance = interestRate / 100 * balance + balance;
-		}
-	}
 }
 
 void Account::interestRate(double interestRate){
@@ -123,17 +40,6 @@ void Account::interestRate(double interestRate){
 void Account::printAccounts() {
 	cout << "Account number: " << number << endl;
 	cout << "Balance: " << balance << endl;;
-}
-
-void Account:: exit(int accountTotal)
-{
-	for (int i = 0; i < accountTotal; i++) {
-		if (number > 1000) {
-			number = 0;
-			balance = 0;
-		}
-	}
-	std::exit(0);
 }
 
 void Account::exit() {
